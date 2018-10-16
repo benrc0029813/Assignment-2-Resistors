8e1dabaaaa97dff365bc0474162cbc00465dce0a
Benjamin David Roberts <rc0029813@student.riversidecollege.ac.uk>
2018-10-16 10:42:20 +0100

    I have entered all the  necessary code for total resistance calculated for a  parallel circuit with two resistors. In addition  to inserting all the necessary parts and code needed for the total resistance to be calculate in a series circuit with three resistors. Moreover I also have created a fully working drop down menu.

--- Assisnment 2 Resistors/Form1.Designer.cs
+++ Assisnment 2 Resistors/Form1.Designer.cs
@@ -40,11 +40,20 @@
             this.label6 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(154, 110);
+            this.textBox1.Location = new System.Drawing.Point(168, 110);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
@@ -59,7 +68,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(176, 94);
+            this.label1.Location = new System.Drawing.Point(189, 94);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(54, 13);
             this.label1.TabIndex = 2;
@@ -85,7 +94,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(213, 136);
+            this.button1.Location = new System.Drawing.Point(217, 136);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 23);
             this.button1.TabIndex = 5;
@@ -101,6 +110,7 @@
             this.button2.TabIndex = 6;
             this.button2.Text = "Calculate";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label4
             // 
@@ -143,11 +153,105 @@
             this.textBox4.Size = new System.Drawing.Size(100, 20);
             this.textBox4.TabIndex = 11;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Series Circuit 2",
+            "Parallel Cicuit 2",
+            "Series Circuit 3",
+            "Parallel Cicuit 3",
+            "Series Circuit 4",
+            "Parallel Cicuit 4",
+            "Series Circuit 5",
+            "Parallel Cicuit 5"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 67);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(189, 94);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Resistor 1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(296, 94);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Resistor 2";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(402, 94);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(54, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Resistor 3";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(168, 110);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 16;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(274, 136);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Calculate";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(486, 113);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(87, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Total Resistance";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(274, 110);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 19;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(380, 110);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 20;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(839, 550);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label6);
@@ -181,6 +285,15 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
     }
 }
 
--- Assisnment 2 Resistors/Form1.cs
+++ Assisnment 2 Resistors/Form1.cs
@@ -15,6 +15,7 @@ namespace Assisnment_2_Resistors
         public Form1()
         {
             InitializeComponent();
+            comboBox1.SelectedIndex = 0;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -29,6 +30,98 @@ namespace Assisnment_2_Resistors
             catch
             {
                 label3.Text = "Error unable to calculate.";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double D3 = Convert.ToDouble(textBox3.Text);
+                double D4 = Convert.ToDouble(textBox4.Text);
+                double TotalResistance = (D3 * D4) / (D3 + D4);
+                label6.Text = TotalResistance.ToString();
+            }
+            catch
+            {
+                label6.Text = "Error unable to calculate.";
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                HideAll();
+                textBox1.Show();
+                textBox2.Show();
+                label1.Show();
+                label2.Show();
+                label3.Show();
+                button1.Show();
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                HideAll();
+                textBox3.Show();
+                textBox4.Show();
+                label4.Show();
+                label5.Show();
+                label6.Show();
+                button2.Show();
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                HideAll();
+                textBox5.Show();
+                textBox6.Show();
+                textBox7.Show();
+                label7.Show();
+                label8.Show();
+                label9.Show();
+                label10.Show();
+                button3.Show();
+            }
+        }
+
+        private void HideAll()
+        {
+            textBox1.Hide();
+            textBox2.Hide();
+            textBox3.Hide();
+            textBox4.Hide();
+            textBox5.Hide();
+            textBox6.Hide();
+            textBox7.Hide();
+            label1.Hide();
+            label2.Hide();
+            label3.Hide();
+            label4.Hide();
+            label5.Hide();
+            label6.Hide();
+            label7.Hide();
+            label8.Hide();
+            label9.Hide();
+            label10.Hide();
+            button1.Hide();
+            button2.Hide();
+            button3.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double D5 = Convert.ToDouble(textBox5.Text);
+                double D6 = Convert.ToDouble(textBox6.Text);
+                double D7 = Convert.ToDouble(textBox7.Text);
+                double TotalResistance = D5 + D6 + D7;
+                label10.Text = TotalResistance.ToString();
+            }
+            catch
+            {
+                label10.Text = "Error unable to calculate.";
             }
         }
     }
